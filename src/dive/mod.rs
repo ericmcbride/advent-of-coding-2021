5f75ecca4f928f13ac62a943460cc299f048de42
Eric McBride <eric.mcbride@toyotaconnected.com>
2021-12-11 21:40:40 -0600

    update

---
+++ src/dive/mod.rs
@@ -0,0 +1,52 @@
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum DiveDirection {
+    Up,
+    Down,
+    Forward,
+}
+
+impl FromStr for DiveDirection {
+    type Err = ();
+    fn from_str(input: &str) -> Result<DiveDirection, Self::Err> {
+        match input {
+            "up" => Ok(DiveDirection::Up),
+            "down" => Ok(DiveDirection::Down),
+            "forward" => Ok(DiveDirection::Forward),
+            _ => Err(()),
+        }
+    }
+}
+
+fn horizontal_and_depth() -> Result<String, crate::Error> {
+    let input = crate::load_input("horizontal_depth.txt");
+    let mut depth: u32 = Default::default();
+    let mut horizontal: u32 = Default::default();
+
+    for i in input.lines() {
+        let line_split = i.split(' ').collect::<Vec<&str>>();
+        let spaces_moved = line_split.get(1).unwrap().parse::<u32>().unwrap();
+        let direction = DiveDirection::from_str(line_split.get(0).unwrap()).unwrap();
+        match direction {
+            DiveDirection::Up => depth -= spaces_moved,
+            DiveDirection::Down => depth += spaces_moved,
+            DiveDirection::Forward => horizontal += spaces_moved,
+        }
+    }
+
+    let total = depth * horizontal;
+    Ok(total.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let got = horizontal_and_depth();
+        assert!(got.is_ok());
+        assert_eq!(got.unwrap(), String::from("1936494"));
+    }
+}
